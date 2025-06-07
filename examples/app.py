cc85b2553571478fee12040a866b04d5faf93573
Chris Holden <ceholden@gmail.com>
2025-06-07 00:46:50 -0400

    feat: rotate credential to SSM

---
+++ examples/app.py
@@ -0,0 +1,35 @@
+"""
+Example CDK application using the EarthdataCredentialCache construct
+"""
+
+import os
+from typing import Any
+
+from aws_cdk import App, Tags, Stack
+from constructs import Construct
+
+from edl_credential_rotation import EarthdataCredentialCache
+
+stack_name = os.environ["STACK_NAME"]
+
+
+class EdlCredentialCacheStack(Stack):
+    """EDL Credentials Cache CDK stack."""
+
+    def __init__(self, scope: Construct, stack_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, f"{stack_id}App", **kwargs)
+        self.edl_credential_cache = EarthdataCredentialCache(self, stack_id)
+
+
+app = App()
+stack = EdlCredentialCacheStack(
+    app,
+    stack_name,
+)
+
+for k, v in dict(
+    Stack=stack_name,
+).items():
+    Tags.of(app).add(k, v)
+
+app.synth()
